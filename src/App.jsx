d6a1568c48221bc5c9b8a5de72ec3086ecf0dbef
Ayush Singh <ayushsingh.xi@gmail.com>
2026-04-17 02:45:55 +0530

    done for the day

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import About from "./pages/About";
+
 const App = () => {
   return (
     <>
-    
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
