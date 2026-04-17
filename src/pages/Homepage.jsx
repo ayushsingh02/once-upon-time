d06d206dba9d95d89cbdf421254f84da4384ae46
Ayush Singh <ayushsingh.xi@gmail.com>
2026-04-18 00:59:58 +0530

    header

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
 
 const Homepage = () => {
   return (
-    <div>homepage</div>
-  )
-}
+    <div>
+      <header>
+        <div className="header desktop">
+          <div className="container">
+            <div className="header-in">
+              <div className="left">
+                <div className="header-logo">
+                  <img
+                    src="../src/assets/images/header-logo.svg"
+                    alt="Logo"
+                    className="icon"
+                  />
+                </div>
+              </div>
+              <div className="right">
+                <nav>
+                  <ul>
+                    <li>
+                      <a href="#!">Therapy</a>
+                    </li>
+                    <li>
+                      <a href="#!">Collaborate</a>
+                    </li>
+                    <li>
+                      <a href="#!">Therapy</a>
+                    </li>
+                    <li>
+                      <a href="#!">About</a>
+                    </li>
+                    <li>
+                      <a href="#!">Stories</a>
+                    </li>
+                    <li>
+                      <a href="#!">Book A Call</a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
