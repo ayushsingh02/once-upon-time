9b90621b3c379d21d0892908cf7b74b00507ae56
Ayush Singh <ayushsingh.xi@gmail.com>
2026-04-22 02:45:51 +0530

    okay

--- src/components/sections/HomeChapter2.jsx
+++ src/components/sections/HomeChapter2.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import WayWorkCard from '../snippets/WayWorkCard'
+import React from "react";
+import WayWorkCard from "../snippets/WayWorkCard";
 
 const HomeChapter2 = () => {
   return (
-    <div>
-<WayWorkCard></WayWorkCard>
+    <div className="home-chapter-2">
+      <div className="heading">
+        <div className="heading-left">
+          <p className="eyebrow-header">Chapter II</p>
+        </div>
+        <div className="heading-right">
+          <p><span className="big-pink">W</span>e believe therapy should feel deeply human—never cold or clinical.Our work is soft, collaborative, and rooted in emotional storytelling,guiding you back to connection with yourself and the people around you.</p>
+        </div>
+      </div>
+      <WayWorkCard></WayWorkCard>
     </div>
-  )
-}
+  );
+};
 
-export default HomeChapter2
+export default HomeChapter2;
